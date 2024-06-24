80c1f42e69c5c0d1d4ded88d54fd064d0b764af8
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-06-24 18:34:06 +0200

    Ajouté une fonction minutes_en_heures qui convertir une durée de séance exprimée en minutes en une durée en heures et minutes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 
 
 
-date_actuelle = datetime.datetime.today()
+date_actuelle = datetime.datetime.today() # Obtenir la date actuelle afin de déterminer quel est le trimestre actuel
 print(date_actuelle)
 
 
@@ -28,6 +28,11 @@ def toJSON(text_data):
     return donnees_json
 
 
+def minutes_en_heures(minutes=60):
+    "Convertir le temps d'une séance de minutes en heures"
+    heures = minutes // 60 # Calculer le nombre d'heures en faisant une division entière de la séance en minutes
+    minutes_restantes = minutes % 60 # Rajouter le nombre de minutes restantes au nombre d'heures
+    return (heures, minutes_restantes)
     
 
 
@@ -39,7 +44,7 @@ def find_id_prog():
         id_prog = 0
         reponse = requests.get("https://api.cnmtq.fr/progs") # Faire une requête de données JSON à l'API
         code_reponse = reponse.status_code # Code de la réponse (par exemple, 200)
-        print("Le serveur a répondu avec le code ", code_reponse)
+        print("Le serveur a répondu avec le code ", code_reponse) # Afficher le code renvoyé par le serveur
         texte = reponse.text # Texte contenu dans la ressource
         donnees_json = toJSON(texte) # Texte converti en JSON
         for dict in donnees_json["data"]: # Pour chaque dictionnaire correspondant à un trimestre
@@ -48,11 +53,11 @@ def find_id_prog():
             date_debut = dict["date_debut"][:10] # Obtenir la date de début du trimestre
             date_fin = dict["date_fin"][:10] # Obtenir la date de fin du trimestre
             #print(f"Le trismetre avec l'identifiant {id_prog} a commencé le {date_debut} et s'est terminé le {date_fin}")
-            if date_actuelle > datetime.datetime.strptime(date_debut, "%Y-%m-%d") and date_actuelle < datetime.datetime.strptime(date_fin, "%Y-%m-%d"):
-                print(f"Le trimestre actuel a l'ID {id_prog}")
-                break
+            if date_actuelle > datetime.datetime.strptime(date_debut, "%Y-%m-%d") and date_actuelle < datetime.datetime.strptime(date_fin, "%Y-%m-%d"): # Si la date actuelle est comprise entre la date début et la date de fin du trimestre
+                print(f"Le trimestre actuel a l'ID {id_prog}") # On en déduit l'identifiant est celui du trimestre actuel
+                break # Une fois l'identifiant du trimestre actuel trouvé, quitter la boucle
 
-        return id_prog    
+        return id_prog # Retourner l'identifiant du trimestre actuel
 
         
 
@@ -60,7 +65,7 @@ def find_id_prog():
         print("Impossible de se connecter au serveur.")
 
     except reqexcpt.Timeout: # Si la requête a expiré
-        print("La requête n'a pas aboutit.")    
+        print("La requête n'a pas aboutit car le serveur a mis trop de temps à répondre.")    
         
 
 
@@ -82,7 +87,7 @@ def trouver_seances(id_prog, date_filter=None):
         print("Impossible de se connecter au serveur.")
 
     except reqexcpt.Timeout: # Si la requête a expiré
-        print("La requête n'a pas aboutit.")    
+        print("La requête n'a pas aboutit car le serveur a mis trop de temps à répondre.")    
 
 
 
@@ -93,7 +98,7 @@ id_prog = find_id_prog() # Envoyer une requête à l'API de la Cinémathèque
 
 date = date_actuelle
 for arg in sys.argv: # Pour chaque argument donné au script
-    if arg.startswith("date_filter=") and arg[:12] is not "": # Si l'argument spécifie le filtre des séances par années
+    if arg.startswith("date_filter=") and arg[:12] !="": # Si l'argument spécifie le filtre des séances par années
         date = datetime.datetime.strptime(arg[12:], "%Y-%m-%d")
 
 
@@ -105,13 +110,18 @@ seances = trouver_seances(id_prog, date_filter=date) # Trouver toutes les séanc
 
 
 if len(seances) > 0:
+    print(f"Les séances ont été programmées pour la date du {date} :")
+    print() # Faire un saut à la ligne
     for seance in seances:
         for item in seance["items"]: 
+            duree = minutes_en_heures(item["duree"]) # Durée de la séance convertie en heures
+            heures = duree[0] # Durée en heures
+            minutes = duree[1] # Durée en minutes
             if "realisateurs" in item:
-                print(item["titre"], f"({item["realisateurs"]})", seance["dateHeure"])
+                print("Titre :", item["titre"], ", Realisateur(s) :", f"{item["realisateurs"]}", ", Horaires de la séance :", seance["dateHeure"], f", Durée de la séance : {heures} heure(s) {minutes} minutes")
 
             else:
-                print(item["titre"], seance["dateHeure"])
+                print("Titre :", item["titre"], ", Horaires de la séance :", seance["dateHeure"], f", Durée de la séance : {heures} heures {minutes} minutes")
 
 else:
     print(f"Aucune séance n'a été programmée pour la date du {date if date_actuelle != date else date_actuelle}")                    
